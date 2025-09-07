04047c12dd3b75e9be43abebc6b6197d95f60889
Calvin Nordstrom <calnordstrom@icloud.com>
2025-09-07 17:46:02 -0400

    feat: add event system

--- CNEngine/src/CNEngine/Application.cpp
+++ CNEngine/src/CNEngine/Application.cpp
@@ -1,11 +1,17 @@
 #include "Application.h"
 
+#include "CNEngine/Events/ApplicationEvent.h"
+#include "CNEngine/Log.h"
+
 namespace CNEngine {
 	Application::Application() {}
 
 	Application::~Application() {}
 
 	void Application::Run() {
+		WindowResizeEvent e(1280, 720);
+		CNE_CORE_TRACE(e.ToString());
+
 		while (true);
 	}
 }
