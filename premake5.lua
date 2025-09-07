2957fc05e82432d384da0ff90fa5cb049d1b6410
Calvin Nordstrom <calnordstrom@icloud.com>
2025-09-07 10:40:34 -0400

    feat: add premake build system

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "CNEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CNEngine"
+	location "CNEngine"
+	kind "SharedLib"
+	language "C++"
+	buildoptions{"/utf-8"}
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CNE_PLATFORM_WINDOWS",
+			"CNE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CNE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CNE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CNE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	buildoptions{"/utf-8"}
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"CNEngine/vendor/spdlog/include",
+		"CNEngine/src"
+	}
+
+	links
+	{
+		"CNEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CNE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CNE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CNE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CNE_DIST"
+		optimize "On"
